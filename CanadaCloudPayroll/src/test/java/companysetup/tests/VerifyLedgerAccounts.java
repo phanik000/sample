621ed08c4626b931e73936faf6dadb82810ca965
phaniravi <45285563+phaniravi@users.noreply.github.com>
2019-08-02 18:24:04 +0530

    Update VerifyLedgerAccounts.java

--- CanadaCloudPayroll/src/test/java/companysetup/tests/VerifyLedgerAccounts.java
+++ CanadaCloudPayroll/src/test/java/companysetup/tests/VerifyLedgerAccounts.java
@@ -36,51 +36,74 @@ public class VerifyLedgerAccounts extends BaseClass {
 
 		
 		
-		s = new Select(ledger.SelectPayrollCategoriesLedger);
+			s = new Select(ledger.SelectPayrollCategoriesLedger);
 		List<WebElement> payrollcategory = s.getOptions();
 		int payrollsize = payrollcategory.size();
-		System.out.println("payroll size is:" +payrollsize);
-		
-		
-		
-		for (int k = 0; k<payrollsize; k++) {
+		System.out.println("payroll size is:" + payrollsize);
+
+		for (int k = 0; k < payrollsize; k++) {
 			WebElement e = payrollcategory.get(k);
-			
 			System.out.println("payroll category text is:" + e.getText());
 			String data1 = excel.getCellData(path, "ledger accounts", k + 1, 0);
 			s.selectByVisibleText(data1);
 			Thread.sleep(5000);
-		
-		
-		size =ledger.EditActionBtnLedger.size();
-		System.out.println("edit size is: "+size);
-		for (int i =0; i<size; i++) {
-			 el =ledger.EditActionBtnLedger.get(i);
-			Thread.sleep(1000);
-				el.click();		
-		}
-		int acsize=ledger.EnterLedgerAccountNumbers.size();
-		int save=ledger.SaveEditAccountNo.size();
-        System.out.println("ac no size is: "+acsize);
-        System.out.println(" save ac size is: "+acsize);
 
-		for(int j=0;j<save;j++) {
-			WebElement enteracno=ledger.EnterLedgerAccountNumbers.get(j);
-			Thread.sleep(1000);
-			enteracno.clear();
-			enteracno.sendKeys(RandomStringUtils.randomNumeric(6));
-			Thread.sleep(2000);
-			
-			WebElement savecheck =ledger.SaveEditAccountNo.get(j);
-			savecheck.click();
-			Thread.sleep(3000);
-		}
-		}
+			int size;
+			try {
+				size = ledger.EditActionBtnLedger.size();
+			} catch (Exception ex) {
+				size = ledger.EditActionBtnLedgerStale.size();
+				System.out.println("stale excep is: " + ex);
+			}
+			System.out.println("edit size is: " + size);
+			for (int i = 0; i < size; i++) {
+				WebElement el;
+				try {
+					el = ledger.EditActionBtnLedger.get(i);
+					el.click();
+				} catch (Exception e1) {
+					System.out.println("excep is: " + e1);
+					el = ledger.EditActionBtnLedgerStale.get(i);
+					el.click();
+				}
+				Thread.sleep(1000);
 
+			}
+			int acsize = ledger.EnterLedgerAccountNumbers.size();
+
+			int save = ledger.SaveEditAccountNo.size();
+			System.out.println("ac no size is: " + acsize);
+			System.out.println(" save ac size is: " + acsize);
+
+			for (int j = 0; j < save; j++) {
+				try {
+					enteracno = ledger.EnterLedgerAccountNumbers.get(j);
+					enteracno.clear();
+					enteracno.sendKeys(RandomStringUtils.randomNumeric(6));
+					Thread.sleep(2000);
+				} catch (Exception e1) {
+					enteracno = ledger.EnterLedgerAccountNumbersStale.get(j);
+
+					enteracno.clear();
+					enteracno.sendKeys(RandomStringUtils.randomNumeric(6));
+					Thread.sleep(2000);
+					e1.printStackTrace();
+				}
+				Thread.sleep(1000);
+				try {
+					WebElement savecheck = ledger.SaveEditAccountNo.get(j);
+					savecheck.click();
+				} catch (Exception e1) {
+					WebElement savecheck = ledger.SaveEditAccountNoStale.get(j);
+					savecheck.click();
+					e1.printStackTrace();
+				}
+				Thread.sleep(3000);
+			}
+		}
 	}
-	{
-		
-	}
+
+
 	
 	
 	
